c65c4de06103bad45ca0eb1361648b616ca8fea6
ValentinChevrier <piedbouche.prod@gmail.com>
2023-01-13 22:55:30 +0100

    Last push du VENDREDI
    Let's go sheesh ou pas mais manque pas mal de truc zebi

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,21 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_AMB_FIREFLY_LP = 3330973287U;
+        static const AkUniqueID PLAY_AMB_FISHWATER_SPLASH = 4238171894U;
+        static const AkUniqueID PLAY_AMB_RIVER_LP = 2485890452U;
+        static const AkUniqueID PLAY_AMB_SPACESHIP_LP = 1481084790U;
+        static const AkUniqueID PLAY_AMB_WIND_LP = 3017133910U;
         static const AkUniqueID PLAY_BOSS_BASIC_ATTACK_CAST = 1913908769U;
         static const AkUniqueID PLAY_BOSS_BASIC_ATTACK_CHARGE = 2001443272U;
         static const AkUniqueID PLAY_BOSS_DEATH = 3695176782U;
+        static const AkUniqueID PLAY_BOSS_FOLEY_IDLE = 3768099550U;
+        static const AkUniqueID PLAY_BOSS_FOLEY_TURN_45 = 1120149441U;
+        static const AkUniqueID PLAY_BOSS_FOLEY_TURN_90 = 935595667U;
+        static const AkUniqueID PLAY_BOSS_FOLEY_TURN_135 = 1773571501U;
+        static const AkUniqueID PLAY_BOSS_FOLEY_TURN_180 = 1689683337U;
         static const AkUniqueID PLAY_BOSS_FOLEY_WALK = 2153254035U;
+        static const AkUniqueID PLAY_BOSS_FOOTSTEP_ANIM = 369227166U;
         static const AkUniqueID PLAY_BOSS_FOOTSTEP_WALK = 1247894122U;
         static const AkUniqueID PLAY_BOSS_GET_HIT = 751032866U;
         static const AkUniqueID PLAY_BOSS_GRENADE_BOUNCE = 4035912257U;
@@ -24,7 +35,33 @@ namespace AK
         static const AkUniqueID PLAY_BOSS_GRENADE_EXPLOSION = 3417853078U;
         static const AkUniqueID PLAY_BOSS_SHIELD = 251293067U;
         static const AkUniqueID PLAY_BOSS_SHOCKWAVE = 4180052369U;
+        static const AkUniqueID PLAY_RFX_BIRDS = 1559944383U;
+        static const AkUniqueID PLAY_RFX_BIRDSTHREE = 401297769U;
+        static const AkUniqueID PLAY_RFX_BIRDSTWO = 350208747U;
+        static const AkUniqueID PLAY_RFX_WALKINGOUT = 2770071110U;
+        static const AkUniqueID PLAY_UI_PARAMETER_ACCEPT = 2039095075U;
+        static const AkUniqueID PLAY_UI_PARAMETER_APPEAR = 3674615292U;
+        static const AkUniqueID PLAY_UI_PARAMETER_DECLINE = 1714108051U;
+        static const AkUniqueID PLAY_UI_PARAMETER_HOVER = 2705587887U;
+        static const AkUniqueID PLAY_UI_PARAMETER_MOVE = 2531204684U;
+        static const AkUniqueID PLAY_UI_PARAMETER_RESUME = 1035094046U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace OPTION_OFF_ON
+        {
+            static const AkUniqueID GROUP = 3567003756U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace OPTION_OFF_ON
+
+    } // namespace STATES
 
     namespace BANKS
     {
@@ -32,6 +69,7 @@ namespace AK
         static const AkUniqueID EXT = 898834102U;
         static const AkUniqueID GLOBAL = 1465331116U;
         static const AkUniqueID INT = 529034928U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BANKS
 
     namespace BUSSES
@@ -44,6 +82,13 @@ namespace AK
         static const AkUniqueID UI = 1551306167U;
         static const AkUniqueID VOICE = 3170124113U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB_BIRDS_FAR = 2650413450U;
+        static const AkUniqueID REVERB_MONSTERBIRDS_FAR = 2729476904U;
+        static const AkUniqueID REVERBS = 3545700988U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
